94de2de67ce29515ea2070694091ed6b4ffe2ee0
NANDAJANAIR <nandajanair.200410107081@gmail.com>
2023-09-02 02:52:35 +0530

    ijjj

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoDB = require("./db")
+var cors = require('cors')
+app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
